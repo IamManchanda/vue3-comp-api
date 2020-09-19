2f45367d812673fb4b2676b4974be70f37f79031
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-19 19:18:53 +0530

    Vue 3 Composition API first implementation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>Capacity: {{ capacity }}</p>
+    <button @click="increaseCapacity">Increase Capacity</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import { ref } from "vue";
+export const capacity = ref(0);
+export const increaseCapacity = () => (capacity.value += 1);
 </script>
