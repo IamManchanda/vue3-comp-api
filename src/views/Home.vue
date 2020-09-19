2328f087fc0bca7eb4dce6f064f9bc32adb9fec4
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-19 19:26:37 +0530

    Computed Properties in Composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-    <p>Capacity: {{ capacity }}</p>
+    <p>Spaces left: {{ spacesLeft }} out of {{ capacity }}</p>
     <button @click="increaseCapacity">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, nameIdx) in attending" :key="nameIdx">
+        {{ name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-export const capacity = ref(0);
+import { ref, computed } from "vue";
+export const capacity = ref(3);
+export const attending = ref(["Tim", "Bob", "Joe"]);
+export const spacesLeft = computed(() => {
+  return capacity.value - attending.value.length;
+});
 export const increaseCapacity = () => (capacity.value += 1);
 </script>
