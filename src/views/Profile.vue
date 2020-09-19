99e74ecd0fe1e379b0e96639c9e0e42819f5f0f3
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-19 23:46:11 +0530

    Error Handling

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,25 @@
 <template>
-  <profile-card />
+  <div>
+    <div v-if="error">There is an error: {{ error }}</div>
+    <profile-card v-else />
+  </div>
 </template>
 
 <script>
+import { ref, onErrorCaptured } from "vue";
 import ProfileCard from "@/components/profile-card";
 
 export default {
   components: {
     ProfileCard,
+  },
+  setup() {
+    const error = ref(null);
+    onErrorCaptured(e => {
+      error.value = e;
+      return true;
+    });
+    return { error };
   },
 };
 </script>
